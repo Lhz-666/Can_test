4ffc292b182975bc377aee217c96d690cbdb1219
LHZ <2398158165@qq.com>
2025-09-07 15:41:15 +0800

    云台代码（可接收云台上电机的信息，发送只验证了yaw轴和两个摩擦轮）

--- Inc/main.h
+++ Inc/main.h
@@ -37,6 +37,7 @@ extern "C" {
 /* Exported types ------------------------------------------------------------*/
 /* USER CODE BEGIN ET */
 extern CAN_HandleTypeDef hcan1;
+extern CAN_HandleTypeDef hcan2;
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
--- Src/freertos.c
+++ Src/freertos.c
@@ -26,6 +26,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "usart.h"
+#include "PID.h"
 //void can_filter_init(void);
 /* USER CODE END Includes */
 
@@ -46,8 +47,37 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
-uint16_t GetSpeed,SetSpeed,GetRpm,GetCurrent,GetTemperature;
-int16_t speed=9000;
+/*pitch电机*/
+int16_t pitch_GetSpeed=0,pitch_GetAngle=0,pitch_GetCurrent=0,pitch_GetTemperature=0;
+int16_t pitch_Last_Get_Real_Angle=0,pitch_Get_Real_Angle=0;//获取的真实值
+/*yaw电机*/
+int16_t yaw_GetSpeed=0,yaw_GetAngle=0,yaw_GetCurrent=0,yaw_GetTemperature=0;
+int16_t yaw_Last_Get_Real_Angle=0,yaw_Get_Real_Angle=0;
+/*左摩擦轮*/
+int16_t left_mocan_GetSpeed=0,left_mocan_GetAngle=0,left_mocan_GetCurrent=0,left_mocan_GetTemperature=0;
+int16_t left_mocan_Last_Get_Real_Angle=0,left_mocan_Get_Real_Angle=0;
+/*右摩擦轮*/
+int16_t right_mocan_GetSpeed=0,right_mocan_GetAngle=0,right_mocan_GetCurrent=0,right_mocan_GetTemperature=0;
+int16_t right_mocan_Last_Get_Real_Angle=0,right_mocan_Get_Real_Angle=0;
+/*拨盘电机*/
+int16_t bopan_GetSpeed=0,bopan_GetAngle=0,bopan_GetCurrent=0,bopan_GetTemperature=0;
+int16_t bopan_Last_Get_Real_Angle=0,bopan_Get_Real_Angle=0;
+
+int16_t SetVoltage=12500;//这里是电压值。电压给定值范围：-25000~0~25000, 对应最大转矩电流范围 -3A~0~3A
+int8_t pitch_number1,yaw_number2;//这里是圈数
+
+int16_t setspeed=0;
+int16_t setposition=0;
+
+extern uint8_t rx_buffer[32];
+
+extern uint16_t channels[32];
+
+MOTOR_t pitch_motor_6020,yaw_motor_6020,left_mocan_2006,right_mocan_2006,bopan_3508;
+
+//int pitch_motor_give_Voltage=0,yaw_give_Voltage=0,left_mocan_give_Voltage=0,right_mocan_give_Voltage=0,bopan_give_Voltage=0;
+
+//uint16_t pitch
 /* USER CODE END Variables */
 osThreadId defaultTaskHandle;
 osThreadId printf_dataHandle;
@@ -129,27 +159,102 @@ void MX_FREERTOS_Init(void) {
 void StartDefaultTask(void const * argument)
 {
   /* USER CODE BEGIN StartDefaultTask */
+	MOTOR_Init(&pitch_motor_6020);
+	MOTOR_Init(&yaw_motor_6020);
+	MOTOR_Init(&left_mocan_2006);
+	MOTOR_Init(&right_mocan_2006);
+	MOTOR_Init(&bopan_3508);
+	channels[0]=1500;
+	channels[1]=1500;
+	channels[2]=1500;
+	channels[3]=1500;
+	channels[4]=1500;
   /* Infinite loop */
 	for(;;)//这段CMD代码一定要放在循环中否则动不了。还有就是由于这里没有电调，所以直接用的是电机的标识符
   {
+    if(channels[0]==1500&&channels[1]==1501&&channels[2]==1500&&channels[3]==1498&&channels[4]==1000&&channels[5]==1000)
+		{
+			pitch_motor_6020.give_Voltage=0;
+			yaw_motor_6020.give_Voltage=0;
+			left_mocan_2006.give_Voltage=0;
+			right_mocan_2006.give_Voltage=0;
+			bopan_3508.give_Voltage=0;
+		}
+		else
+		{
+			/*摩擦轮*/
+			left_mocan_2006.Set_speed=(channels[1]-1500)*6000/500;
+//			left_mocan_2006.Set_speed=setspeed;
+			left_mocan_2006.speed=left_mocan_GetSpeed;//速度环
+			left_mocan_2006.give_Voltage=(int16_t)PID_Speed_Calculate(&left_mocan_2006.speed_PID,left_mocan_2006.speed,left_mocan_2006.Set_speed);
+				
+		right_mocan_2006.Set_speed=(channels[1]-1500)*6000/500;
+//			right_mocan_2006.Set_speed=setspeed;
+			right_mocan_2006.speed=right_mocan_GetSpeed;//速度环
+			right_mocan_2006.give_Voltage=(int16_t)PID_Speed_Calculate(&right_mocan_2006.speed_PID,right_mocan_2006.speed,right_mocan_2006.Set_speed);
+    /*pitch和yaw轴*/
+		pitch_motor_6020.position=pitch_GetAngle;//位置环(能控但是有点慢且不准)//高低
+		pitch_motor_6020.Set_position=(channels[2]-1000)*30/1000;
+		pitch_motor_6020.give_Voltage=(int16_t)PID_Position_Calculate(&pitch_motor_6020.position_PID,pitch_motor_6020.position,pitch_motor_6020.Set_position);
+			
+		yaw_motor_6020.position=yaw_GetAngle;//位置环(能控但是有点慢且不准)//左右
+		yaw_motor_6020.Set_position=(channels[0]-1000)*360/1000;
+		yaw_motor_6020.give_Voltage=(int16_t)PID_Position_Calculate(&yaw_motor_6020.position_PID,yaw_motor_6020.position,yaw_motor_6020.Set_position);
+		}
+		/*控制pitch电机电压(CAN1)*/
     CAN_TxHeaderTypeDef pHeader1;
-		pHeader1.StdId = 0x1FF;       //��ʶ����0x205
-		pHeader1.IDE = CAN_ID_STD;    //֡���ͣ���׼֡
-		pHeader1.RTR = CAN_RTR_DATA;  //֡��ʽ��DATA
-		pHeader1.DLC = 0x08;          //DLC��8�ֽ�
-		
+		pHeader1.StdId = 0x1FF;       
+		pHeader1.IDE = CAN_ID_STD;    
+		pHeader1.RTR = CAN_RTR_DATA;  
+		pHeader1.DLC = 0x08;          
+
 		uint8_t aData[8]={0};
-		int16_t speed=10000;
-		aData[0] = speed >> 8;          //���ID��1�����Ƶ���ֵ��8λ
-		aData[1] = speed;               //���ID��1�����Ƶ�����8λ
-		aData[2] = 0;          //���ID��1�����Ƶ���ֵ��8λ
-		aData[3] = 0; 
-		aData[4] = 0;          //���ID��1�����Ƶ���ֵ��8λ
+		aData[0] = 0;          
+		aData[1] = 0;		
+		aData[2] = pitch_motor_6020.give_Voltage >> 8;          
+		aData[3] = pitch_motor_6020.give_Voltage ; 
+		aData[4] = 0;          
 		aData[5] = 0; 
-		aData[6] = 0;          //���ID��1�����Ƶ���ֵ��8λ
+		aData[6] = 0;          
 		aData[7] = 0; 
 		
 		HAL_CAN_AddTxMessage(&hcan1, &pHeader1, aData, 0);
+		/*控制左右摩擦轮和拨盘电机电压(CAN1)*/
+		CAN_TxHeaderTypeDef pHeader3;
+		pHeader3.StdId = 0x200;       
+		pHeader3.IDE = CAN_ID_STD;    
+		pHeader3.RTR = CAN_RTR_DATA;  
+		pHeader3.DLC = 0x08;          
+
+		uint8_t cData[8]={0};
+		cData[0] = left_mocan_2006.give_Voltage>>8;          
+		cData[1] = left_mocan_2006.give_Voltage;		
+		cData[2] = right_mocan_2006.give_Voltage >> 8;          
+		cData[3] = right_mocan_2006.give_Voltage ; 
+		cData[4] = bopan_3508.give_Voltage;          
+		cData[5] = bopan_3508.give_Voltage; 
+		cData[6] = 0;          
+		cData[7] = 0; 
+		
+		HAL_CAN_AddTxMessage(&hcan1, &pHeader3, cData, 0);
+		/*控制yaw电机电压(CAN2)*/
+		CAN_TxHeaderTypeDef pHeader2;
+		pHeader2.StdId = 0x1FF;       
+		pHeader2.IDE = CAN_ID_STD;    
+		pHeader2.RTR = CAN_RTR_DATA;  
+		pHeader2.DLC = 0x08;          
+
+		uint8_t bData[8]={0};
+		bData[0] = 0;          
+		bData[1] = 0;  		
+		bData[2] = 0;          
+		bData[3] = 0; 
+		bData[4] = 0;          
+		bData[5] = 0; 
+		bData[6] = yaw_motor_6020.give_Voltage >> 8;          
+		bData[7] = yaw_motor_6020.give_Voltage; 
+		
+		HAL_CAN_AddTxMessage(&hcan2, &pHeader2, bData, 0);
 
   }
   /* USER CODE END StartDefaultTask */
@@ -169,7 +274,8 @@ void StartTask02(void const * argument)
   for(;;)
   {
 //		printf("Rpm:%d,Speed:%d,Current:%d,Temperature:%d\n", GetRpm,GetSpeed,GetCurrent,GetTemperature);  // 两个 %d，两个参数
-		printf("%d,%d,%d,%d\n", GetRpm,GetSpeed,GetCurrent,GetTemperature);  // 两个 %d，两个参数
+		printf("%d,%d,%d\n", left_mocan_GetSpeed,(int)left_mocan_2006.Set_speed,right_mocan_GetSpeed);  // 两个 %d，两个参数
+		HAL_UART_Receive_IT(&huart3,rx_buffer,32);
 		osDelay(1);
   }
   /* USER CODE END StartTask02 */
@@ -186,12 +292,81 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)//回调函数
     switch (rx_header.StdId)//这个以及下面自己发挥
     {
  
-        case 0x205://根据电机具体id号设置 0x204+id(6020手册上找)
+        case 0x206://根据电机具体id号设置 0x204+id(6020手册上找)//pitch轴
         {
-            GetRpm=(uint16_t)((rx_data)[0] << 8 | (rx_data)[1]);
-            GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
-            GetCurrent=(uint16_t)((rx_data)[4] << 8 | (rx_data)[5]);
-            GetTemperature=(uint16_t) (rx_data)[6] ;
+             pitch_Last_Get_Real_Angle=pitch_Get_Real_Angle;
+						pitch_Get_Real_Angle=((uint16_t)((rx_data)[0] << 8 | (rx_data)[1]))*360/8192;
+            pitch_GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
+            pitch_GetCurrent=(uint16_t)((rx_data)[4] << 8 | (rx_data)[5]);
+            pitch_GetTemperature=(uint16_t) (rx_data)[6] ;
+					if(pitch_Last_Get_Real_Angle-pitch_Get_Real_Angle>200)
+						{
+								pitch_number1++;
+						}
+						if(pitch_Last_Get_Real_Angle-pitch_Get_Real_Angle<-200)
+						{
+							pitch_number1--;
+						}
+						pitch_GetAngle=pitch_Get_Real_Angle+pitch_number1*360;
+            break;																										// 此处是将两个数据合并为一个数据
+        }
+				case 0x201://0x200+id左摩擦轮
+				{
+					left_mocan_Get_Real_Angle=left_mocan_Get_Real_Angle;
+						left_mocan_Get_Real_Angle=((uint16_t)((rx_data)[0] << 8 | (rx_data)[1]))*360/8192;
+            left_mocan_GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
+            left_mocan_GetCurrent=(uint16_t)((rx_data)[4] << 8 | (rx_data)[5]);
+            left_mocan_GetTemperature=(uint16_t) (rx_data)[6] ;
+            break;																										// 此处是将两个数据合并为一个数据
+				}
+				case 0x202://右摩擦轮
+				{
+						right_mocan_Get_Real_Angle=right_mocan_Get_Real_Angle;
+						right_mocan_Get_Real_Angle=((uint16_t)((rx_data)[0] << 8 | (rx_data)[1]))*360/8192;
+            right_mocan_GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
+            right_mocan_GetCurrent=(uint16_t)((rx_data)[4] << 8 | (rx_data)[5]);
+            right_mocan_GetTemperature=(uint16_t) (rx_data)[6] ;
+            break;																										// 此处是将两个数据合并为一个数据
+				}
+				case 0x203://拨盘
+				{
+						bopan_Get_Real_Angle=bopan_Get_Real_Angle;
+						bopan_Get_Real_Angle=((uint16_t)((rx_data)[0] << 8 | (rx_data)[1]))*360/8192;
+            bopan_GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
+            bopan_GetCurrent=(uint16_t)((rx_data)[4] << 8 | (rx_data)[5]);
+            bopan_GetTemperature=(uint16_t) (rx_data)[6] ;
+            break;																										// 此处是将两个数据合并为一个数据
+				}
+ 
+    }
+//	GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
+}
+
+void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan)//回调函数
+{
+    CAN_RxHeaderTypeDef rx_header;
+    uint8_t rx_data[8]={0};
+    HAL_CAN_GetRxMessage(&hcan2, CAN_FILTER_FIFO1, &rx_header, rx_data);//将数据存放于rx_data数组中
+ 
+    switch (rx_header.StdId)//这个以及下面自己发挥
+    {
+ 
+        case 0x208://根据电机具体id号设置 0x204+id(6020手册上找)
+        {
+             yaw_Last_Get_Real_Angle=yaw_Get_Real_Angle;
+						yaw_Get_Real_Angle=((uint16_t)((rx_data)[0] << 8 | (rx_data)[1]))*360/8192;
+            yaw_GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
+            yaw_GetCurrent=(uint16_t)((rx_data)[4] << 8 | (rx_data)[5]);
+            yaw_GetTemperature=(uint16_t) (rx_data)[6] ;
+					if(yaw_Last_Get_Real_Angle-yaw_Get_Real_Angle>200)
+						{
+								yaw_number2++;
+						}
+						if(yaw_Last_Get_Real_Angle-yaw_Get_Real_Angle<-200)
+						{
+							yaw_number2--;
+						}
+						yaw_GetAngle=yaw_Get_Real_Angle+yaw_number2*360;
             break;																										// 此处是将两个数据合并为一个数据
         }
  
