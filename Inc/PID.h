38cf5c2f8379b09a4fa10ea7f386c4f7549cfb29
LHZ <2398158165@qq.com>
2025-09-20 11:28:26 +0800

    增加了mpu获取速度

---
+++ Inc/PID.h
@@ -0,0 +1,67 @@
+#include "stdint.h"
+typedef int16_t motor_measure;//改个名字
+
+typedef struct
+{
+    /*PID三参数*/
+    float KP;
+    float KI;
+    float KD;
+
+    float max_out;//最大输出
+    float max_Iout;//最大积分输出
+
+    float Set;//设定值
+    float Feedback;//反馈值
+
+    float out;
+    float Pout;
+    float Iout;
+    float Dout;
+
+    float error[3];//用来存储这次的误差，上次的误差和上上次的误差
+
+}PID_type_def;//用数组的话，不直观就这样改了
+
+/*实例化电机*/
+typedef struct MOTOR
+{
+    const motor_measure motor_measure_data[4];
+    PID_type_def speed_PID;
+    PID_type_def position_PID;
+    float position;
+    float Set_position;
+    float speed;
+    float Set_speed;
+    int give_Voltage;
+}MOTOR_t;
+
+/*功能：PID速度环输出值的计算
+参数:pid（哪个环）
+now_value:现在的值
+set_value:设定值
+返回：PID输出的结果
+*/
+float PID_Speed_Calculate(PID_type_def *pid,float now_value,float set_value);
+/*功能：PID输出值的计算
+参数:pid（哪个环）
+now_value:现在的值
+set_value:设定值
+返回：PID输出的结果
+*/
+float PID_Position_Calculate(PID_type_def *pid,float now_value,float set_value);
+
+/*功能：PID初始化函数
+参数：pid（pid的类型:速度环还是角度环）
+    PID[3]：PID参数
+    max_out 最大输出
+    max_iout 最大积分输出
+返回：无
+*/
+void PID_Init(PID_type_def *pid,const float PID[3],float max_out,float max_iout);
+
+/*功能：MOTOR的PID初始化函数
+参数：motor6020:哪个电机
+返回：无
+*/
+void MOTOR_Init(MOTOR_t *motor6020);
--- Src/freertos.c
+++ Src/freertos.c
@@ -26,6 +26,9 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "usart.h"
+#include "PID.h"
+#include "inv_mpu.h"
+#include "mpu6050.h"
 //void can_filter_init(void);
 /* USER CODE END Includes */
 
@@ -46,11 +49,58 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
-uint16_t GetSpeed,SetSpeed,GetRpm,GetCurrent,GetTemperature;
-int16_t speed=9000;
+/*各个获取的数据*/
+/*pitch电机*/
+int16_t pitch_GetSpeed=0,pitch_GetAngle=0,pitch_GetCurrent=0,pitch_GetTemperature=0;
+int16_t pitch_Last_Get_Real_Angle=0,pitch_Get_Real_Angle=0;//获取的真实值
+/*yaw电机*/
+int16_t yaw_GetSpeed=0,yaw_GetAngle=0,yaw_GetCurrent=0,yaw_GetTemperature=0;
+int16_t yaw_Last_Get_Real_Angle=0,yaw_Get_Real_Angle=0;
+/*左摩擦轮*/
+int16_t left_mocan_GetSpeed=0,left_mocan_GetAngle=0,left_mocan_GetCurrent=0,left_mocan_GetTemperature=0;
+int16_t left_mocan_Last_Get_Real_Angle=0,left_mocan_Get_Real_Angle=0;
+/*右摩擦轮*/
+int16_t right_mocan_GetSpeed=0,right_mocan_GetAngle=0,right_mocan_GetCurrent=0,right_mocan_GetTemperature=0;
+int16_t right_mocan_Last_Get_Real_Angle=0,right_mocan_Get_Real_Angle=0;
+/*拨盘电机*/
+int16_t bopan_GetSpeed=0,bopan_GetAngle=0,bopan_GetCurrent=0,bopan_GetTemperature=0;
+int16_t bopan_Last_Get_Real_Angle=0,bopan_Get_Real_Angle=0;
+short bopan_mode=0;//拨盘的模式，1是单发，2是连发
+/*mpu的数据*/
+float pitch,yaw,roll;
+short pitch_speed,yaw_speed,roll_speed;
+
+int16_t SetVoltage=12500;//这里是电压值。电压给定值范围：-25000~0~25000, 对应最大转矩电流范围 -3A~0~3A
+int8_t pitch_number1,yaw_number2;//这里是圈数
+
+/*实验值*/
+int16_t setspeed=0;
+int16_t pitch_setposition=0;
+int16_t yaw_setposition=0;
+int16_t inner_yaw_setposition=0;
+int16_t bopan_setposition=0;
+
+/*遥控保护标志位*/
+uint8_t remote_protect_flag=0;//0是开启，1是关闭
+
+/*其他的数据*/
+int16_t times=0;//时间遥控保护
+
+extern uint8_t rx_buffer[32];//接收缓冲
+
+extern uint16_t channels[32];//接收通道
+extern uint16_t last_channels[32];
+
+MOTOR_t pitch_motor_6020,inner_pitch_motor_6020,yaw_motor_6020,inner_yaw_motor_6020,left_mocan_2006,right_mocan_2006,bopan_3508,inner_bopan_3508;
+
+
+//int pitch_motor_give_Voltage=0,yaw_give_Voltage=0,left_mocan_give_Voltage=0,right_mocan_give_Voltage=0,bopan_give_Voltage=0;
+
+//uint16_t pitch
 /* USER CODE END Variables */
 osThreadId defaultTaskHandle;
 osThreadId printf_dataHandle;
+osThreadId protectHandle;
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
@@ -59,6 +109,7 @@ osThreadId printf_dataHandle;
 
 void StartDefaultTask(void const * argument);
 void StartTask02(void const * argument);
+void remote_protect(void const * argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -113,6 +164,10 @@ void MX_FREERTOS_Init(void) {
   osThreadDef(printf_data, StartTask02, osPriorityAboveNormal, 0, 128);
   printf_dataHandle = osThreadCreate(osThread(printf_data), NULL);
 
+  /* definition and creation of protect */
+  osThreadDef(protect, remote_protect, osPriorityRealtime, 0, 128);
+  protectHandle = osThreadCreate(osThread(protect), NULL);
+
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
@@ -129,27 +184,153 @@ void MX_FREERTOS_Init(void) {
 void StartDefaultTask(void const * argument)
 {
   /* USER CODE BEGIN StartDefaultTask */
+	MOTOR_Init(&pitch_motor_6020);//赋予相应的PID值以及最大输出值
+	MOTOR_Init(&yaw_motor_6020);
+	MOTOR_Init(&left_mocan_2006);
+	MOTOR_Init(&right_mocan_2006);
+	MOTOR_Init(&bopan_3508);
+	MOTOR_Init(&inner_pitch_motor_6020);
+	MOTOR_Init(&inner_yaw_motor_6020);
+	
+	channels[1]=1500;
+	channels[3]=1500;
+	inner_pitch_motor_6020.give_Voltage=0;//默认上电就给电压为0
+	inner_yaw_motor_6020.give_Voltage=0;
+	left_mocan_2006.give_Voltage=0;
+	right_mocan_2006.give_Voltage=0;
+	bopan_3508.give_Voltage=0;
   /* Infinite loop */
-	for(;;)//这段CMD代码一定要放在循环中否则动不了。还有就是由于这里没有电调，所以直接用的是电机的标识符
+	for(;;)
   {
-    CAN_TxHeaderTypeDef pHeader1;
-		pHeader1.StdId = 0x1FF;       //��ʶ����0x205
-		pHeader1.IDE = CAN_ID_STD;    //֡���ͣ���׼֡
-		pHeader1.RTR = CAN_RTR_DATA;  //֡��ʽ��DATA
-		pHeader1.DLC = 0x08;          //DLC��8�ֽ�
+    //如果正常遥控的话，就计算给电压的值,否则就保护
+		if(remote_protect_flag)
+		{
+			/*摩擦轮*/
+			left_mocan_2006.Set_speed=(channels[1]-1500)*6000/500;
+//			left_mocan_2006.Set_speed=setspeed;
+			left_mocan_2006.speed=left_mocan_GetSpeed;//速度环
+			left_mocan_2006.give_Voltage=(int16_t)PID_Speed_Calculate(&left_mocan_2006.speed_PID,left_mocan_2006.speed,left_mocan_2006.Set_speed);
+				
+		right_mocan_2006.Set_speed=(channels[1]-1500)*6000/500;
+//			right_mocan_2006.Set_speed=setspeed;
+			right_mocan_2006.speed=right_mocan_GetSpeed;//速度环
+			right_mocan_2006.give_Voltage=(int16_t)PID_Speed_Calculate(&right_mocan_2006.speed_PID,right_mocan_2006.speed,right_mocan_2006.Set_speed);
+			
+    /*pitch和yaw轴*/
+//		pitch_motor_6020.position=pitch_GetAngle;//位置环//高低
+//		pitch_motor_6020.Set_position=(channels[2]-1000)*30/1000;
+		pitch_motor_6020.position=pitch;//pitch外环（位置环）
+		pitch_motor_6020.Set_position=pitch_setposition;
+		pitch_motor_6020.give_Voltage=(int16_t)PID_Position_Calculate(&pitch_motor_6020.position_PID,pitch_motor_6020.position,pitch_motor_6020.Set_position);
+		inner_pitch_motor_6020.speed=pitch_GetSpeed;//pitch内环（速度环）
+		inner_pitch_motor_6020.Set_speed=pitch_motor_6020.give_Voltage;
+		inner_pitch_motor_6020.give_Voltage=(int16_t)PID_Speed_Calculate(&inner_pitch_motor_6020.speed_PID,inner_pitch_motor_6020.speed,inner_pitch_motor_6020.Set_speed);
+			
+//		yaw_motor_6020.position=yaw_GetAngle;//位置环//左右
+//		yaw_motor_6020.Set_position=(channels[0]-1000)*360/1000;
+		yaw_motor_6020.position=yaw;//yaw外环（位置环）
+		yaw_motor_6020.Set_position=yaw_setposition;
+		yaw_motor_6020.give_Voltage=(int16_t)PID_Position_Calculate(&yaw_motor_6020.position_PID,yaw_motor_6020.position,yaw_motor_6020.Set_position);
+		inner_yaw_motor_6020.speed=yaw_GetSpeed;//yaw内环（速度环）
+//		inner_yaw_motor_6020.Set_speed=yaw_motor_6020.give_Voltage;
+		inner_yaw_motor_6020.Set_speed=inner_yaw_setposition;
+		inner_yaw_motor_6020.give_Voltage=(int16_t)PID_Speed_Calculate(&inner_yaw_motor_6020.speed_PID,inner_yaw_motor_6020.speed,inner_yaw_motor_6020.Set_speed);
 		
+		/*拨盘电机*/
+//		bopan_3508.position=bopan_GetAngle;//位置环//左右
+//		bopan_3508.Set_position=(channels[3]-1000)*360/1000;//这里的通道尚未确定，先给为3通道	
+
+		
+		if(channels[3]<1200)//设置单发
+		{
+			bopan_mode=1;
+		}
+		else if(channels[3]>1800)//设置连发
+		{
+			bopan_mode=2;
+		}
+		if(bopan_mode==1)
+		{
+			bopan_setposition+=30;
+			bopan_mode=0;
+		}
+		else if(bopan_mode==2)
+		{
+			bopan_setposition+=30;
+		}
+		
+		bopan_3508.position=bopan_GetAngle;//yaw外环（位置环）
+		bopan_3508.Set_position=bopan_setposition;
+		bopan_3508.give_Voltage=(int16_t)PID_Position_Calculate(&bopan_3508.position_PID,bopan_3508.position,bopan_3508.Set_position);
+		inner_bopan_3508.speed=bopan_GetSpeed;//yaw内环（速度环）
+		inner_bopan_3508.Set_speed=bopan_3508.give_Voltage;
+		inner_bopan_3508.give_Voltage=(int16_t)PID_Speed_Calculate(&inner_bopan_3508.speed_PID,inner_bopan_3508.speed,inner_bopan_3508.Set_speed);
+		}
+		else//否则保护
+		{
+			inner_pitch_motor_6020.give_Voltage=0;
+			inner_yaw_motor_6020.give_Voltage=0;
+			left_mocan_2006.give_Voltage=0;
+			right_mocan_2006.give_Voltage=0;
+			bopan_3508.give_Voltage=0;
+		}
+		
+		/*控制pitch电机电压(CAN1)*/
+    CAN_TxHeaderTypeDef pHeader1;
+		pHeader1.StdId = 0x1FF;       
+		pHeader1.IDE = CAN_ID_STD;    
+		pHeader1.RTR = CAN_RTR_DATA;  
+		pHeader1.DLC = 0x08;          
+
 		uint8_t aData[8]={0};
-		int16_t speed=10000;
-		aData[0] = speed >> 8;          //���ID��1�����Ƶ���ֵ��8λ
-		aData[1] = speed;               //���ID��1�����Ƶ�����8λ
-		aData[2] = 0;          //���ID��1�����Ƶ���ֵ��8λ
-		aData[3] = 0; 
-		aData[4] = 0;          //���ID��1�����Ƶ���ֵ��8λ
+		aData[0] = 0;          
+		aData[1] = 0;		
+		aData[2] = inner_pitch_motor_6020.give_Voltage >> 8;          
+		aData[3] = inner_pitch_motor_6020.give_Voltage ; 
+		aData[4] = 0;          
 		aData[5] = 0; 
-		aData[6] = 0;          //���ID��1�����Ƶ���ֵ��8λ
+		aData[6] = 0;          
 		aData[7] = 0; 
 		
 		HAL_CAN_AddTxMessage(&hcan1, &pHeader1, aData, 0);
+		
+		/*控制左右摩擦轮和拨盘电机电压(CAN1)*/
+		CAN_TxHeaderTypeDef pHeader3;
+		pHeader3.StdId = 0x200;       
+		pHeader3.IDE = CAN_ID_STD;    
+		pHeader3.RTR = CAN_RTR_DATA;  
+		pHeader3.DLC = 0x08;          
+
+		uint8_t cData[8]={0};
+		cData[0] = left_mocan_2006.give_Voltage>>8;          
+		cData[1] = left_mocan_2006.give_Voltage;		
+		cData[2] = right_mocan_2006.give_Voltage >> 8;          
+		cData[3] = right_mocan_2006.give_Voltage ; 
+		cData[4] = bopan_3508.give_Voltage;          
+		cData[5] = bopan_3508.give_Voltage; 
+		cData[6] = 0;          
+		cData[7] = 0; 
+		
+		HAL_CAN_AddTxMessage(&hcan1, &pHeader3, cData, 0);
+		
+		/*控制yaw电机电压(CAN2)*/
+		CAN_TxHeaderTypeDef pHeader2;
+		pHeader2.StdId = 0x1FF;       
+		pHeader2.IDE = CAN_ID_STD;    
+		pHeader2.RTR = CAN_RTR_DATA;  
+		pHeader2.DLC = 0x08;          
+
+		uint8_t bData[8]={0};
+		bData[0] = 0;          
+		bData[1] = 0;  		
+		bData[2] = 0;          
+		bData[3] = 0; 
+		bData[4] = 0;          
+		bData[5] = 0; 
+		bData[6] = inner_yaw_motor_6020.give_Voltage >> 8;          
+		bData[7] = inner_yaw_motor_6020.give_Voltage; 
+		
+		HAL_CAN_AddTxMessage(&hcan2, &pHeader2, bData, 0);
 
   }
   /* USER CODE END StartDefaultTask */
@@ -162,21 +343,77 @@ void StartDefaultTask(void const * argument)
 * @retval None
 */
 /* USER CODE END Header_StartTask02 */
-void StartTask02(void const * argument)
+void StartTask02(void const * argument)//打印并关电时间
 {
   /* USER CODE BEGIN StartTask02 */
   /* Infinite loop */
   for(;;)
   {
 //		printf("Rpm:%d,Speed:%d,Current:%d,Temperature:%d\n", GetRpm,GetSpeed,GetCurrent,GetTemperature);  // 两个 %d，两个参数
-		printf("%d,%d,%d,%d\n", GetRpm,GetSpeed,GetCurrent,GetTemperature);  // 两个 %d，两个参数
+		printf("%d,%d,%d,%d,%d,%d\n", yaw_GetSpeed,inner_yaw_setposition,(int)bopan_3508.position,(int)bopan_3508.Set_position,(int)inner_bopan_3508.speed,(int)inner_bopan_3508.Set_speed);  //打印
+		HAL_UART_Receive_IT(&huart3,rx_buffer,32);
+		mpu_dmp_get_data(&pitch,&roll,&yaw);
+		MPU_Get_Gyroscope(&roll_speed,&pitch_speed,&yaw_speed);
+		if(channels[0]==last_channels[0]&&channels[1]==last_channels[1]&&channels[2]==last_channels[2]&&channels[3]==last_channels[3]&&channels[4]==last_channels[4]&&channels[5]==last_channels[5])//时间保护
+		{
+			times++;
+		}
+		else
+		{
+			for(int i=0;i<14;i++)
+			{
+				last_channels[i]=channels[i];
+			}
+			times=0;
+		}
 		osDelay(1);
   }
   /* USER CODE END StartTask02 */
 }
 
+/* USER CODE BEGIN Header_remote_protect */
+/**
+* @brief Function implementing the protect thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_remote_protect */
+void remote_protect(void const * argument)
+{
+  /* USER CODE BEGIN remote_protect */
+  /* Infinite loop */
+  for(;;)
+  {
+    if(channels[0]==1500&&channels[1]==1501&&channels[2]==1500&&channels[3]==1498&&channels[4]==1000&&channels[5]==1000)//遥控保护相关的，判断的值通过遥控器设定(值保护)
+		{
+			inner_pitch_motor_6020.give_Voltage=0;
+			inner_yaw_motor_6020.give_Voltage=0;
+			left_mocan_2006.give_Voltage=0;
+			right_mocan_2006.give_Voltage=0;
+			bopan_3508.give_Voltage=0;
+			remote_protect_flag=0;
+		}
+		else if(times>=1000)//时间遥控保护(时间计算在打印那里)
+		{
+			inner_pitch_motor_6020.give_Voltage=0;
+			inner_yaw_motor_6020.give_Voltage=0;
+			left_mocan_2006.give_Voltage=0;
+			right_mocan_2006.give_Voltage=0;
+			bopan_3508.give_Voltage=0;
+			remote_protect_flag=0;
+		}
+		else
+		{
+			remote_protect_flag=1;
+		}
+		osDelay(1);
+  }
+  /* USER CODE END remote_protect */
+}
+
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
+/*后面这堆函数都是获取数据*/
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)//回调函数
 {
     CAN_RxHeaderTypeDef rx_header;
@@ -186,12 +423,81 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)//回调函数
     switch (rx_header.StdId)//这个以及下面自己发挥
     {
  
-        case 0x205://根据电机具体id号设置 0x204+id(6020手册上找)
+        case 0x206://根据电机具体id号设置 0x204+id(6020手册上找)//pitch轴
         {
-            GetRpm=(uint16_t)((rx_data)[0] << 8 | (rx_data)[1]);
-            GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
-            GetCurrent=(uint16_t)((rx_data)[4] << 8 | (rx_data)[5]);
-            GetTemperature=(uint16_t) (rx_data)[6] ;
+             pitch_Last_Get_Real_Angle=pitch_Get_Real_Angle;
+						pitch_Get_Real_Angle=((uint16_t)((rx_data)[0] << 8 | (rx_data)[1]))*360/8192;
+            pitch_GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
+            pitch_GetCurrent=(uint16_t)((rx_data)[4] << 8 | (rx_data)[5]);
+            pitch_GetTemperature=(uint16_t) (rx_data)[6] ;
+					if(pitch_Last_Get_Real_Angle-pitch_Get_Real_Angle>200)
+						{
+								pitch_number1++;
+						}
+						if(pitch_Last_Get_Real_Angle-pitch_Get_Real_Angle<-200)
+						{
+							pitch_number1--;
+						}
+						pitch_GetAngle=pitch_Get_Real_Angle+pitch_number1*360;
+            break;																										// 此处是将两个数据合并为一个数据
+        }
+				case 0x201://0x200+id左摩擦轮
+				{
+					left_mocan_Get_Real_Angle=left_mocan_Get_Real_Angle;
+						left_mocan_Get_Real_Angle=((uint16_t)((rx_data)[0] << 8 | (rx_data)[1]))*360/8192;
+            left_mocan_GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
+            left_mocan_GetCurrent=(uint16_t)((rx_data)[4] << 8 | (rx_data)[5]);
+            left_mocan_GetTemperature=(uint16_t) (rx_data)[6] ;
+            break;																										// 此处是将两个数据合并为一个数据
+				}
+				case 0x202://右摩擦轮
+				{
+						right_mocan_Get_Real_Angle=right_mocan_Get_Real_Angle;
+						right_mocan_Get_Real_Angle=((uint16_t)((rx_data)[0] << 8 | (rx_data)[1]))*360/8192;
+            right_mocan_GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
+            right_mocan_GetCurrent=(uint16_t)((rx_data)[4] << 8 | (rx_data)[5]);
+            right_mocan_GetTemperature=(uint16_t) (rx_data)[6] ;
+            break;																										// 此处是将两个数据合并为一个数据
+				}
+				case 0x203://拨盘
+				{
+						bopan_Get_Real_Angle=bopan_Get_Real_Angle;
+						bopan_Get_Real_Angle=((uint16_t)((rx_data)[0] << 8 | (rx_data)[1]))*360/8192;
+            bopan_GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
+            bopan_GetCurrent=(uint16_t)((rx_data)[4] << 8 | (rx_data)[5]);
+            bopan_GetTemperature=(uint16_t) (rx_data)[6] ;
+            break;																										// 此处是将两个数据合并为一个数据
+				}
+ 
+    }
+//	GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
+}
+
+void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan)//回调函数
+{
+    CAN_RxHeaderTypeDef rx_header;
+    uint8_t rx_data[8]={0};
+    HAL_CAN_GetRxMessage(&hcan2, CAN_FILTER_FIFO1, &rx_header, rx_data);//将数据存放于rx_data数组中
+ 
+    switch (rx_header.StdId)//这个以及下面自己发挥
+    {
+ 
+        case 0x208://根据电机具体id号设置 0x204+id(6020手册上找)
+        {
+             yaw_Last_Get_Real_Angle=yaw_Get_Real_Angle;
+						yaw_Get_Real_Angle=((uint16_t)((rx_data)[0] << 8 | (rx_data)[1]))*360/8192;
+            yaw_GetSpeed = (uint16_t)((rx_data)[2] << 8 | (rx_data)[3]); // 根据手册 2、3 位分别为电机转速的高八位、低八位
+            yaw_GetCurrent=(uint16_t)((rx_data)[4] << 8 | (rx_data)[5]);
+            yaw_GetTemperature=(uint16_t) (rx_data)[6] ;
+					if(yaw_Last_Get_Real_Angle-yaw_Get_Real_Angle>200)
+						{
+								yaw_number2++;
+						}
+						if(yaw_Last_Get_Real_Angle-yaw_Get_Real_Angle<-200)
+						{
+							yaw_number2--;
+						}
+						yaw_GetAngle=yaw_Get_Real_Angle+yaw_number2*360;
             break;																										// 此处是将两个数据合并为一个数据
         }
  
